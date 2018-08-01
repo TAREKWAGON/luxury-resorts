28b7a40df64c5e9db633f76839858115e0d0e199
Adrian Wright <agwright72@gmail.com>
2018-08-01 17:59:55 +0700

    seeds added

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/show'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
-  get 'resorts/home'
-  get 'resorts/index'
-  get 'resorts/show'
-
   resources :resorts, only: [:home, :index, :show] do
     resources :bookings, only: [:create]
   end
