0e13e1ac77ac7a9ab9fdd5ee4513abffe040df15
Leanne Egan <lmegan92@gmail.com>
2018-08-01 15:56:08 +0800

    created resorts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'resorts/home'
+  get 'resorts/index'
+  get 'resorts/show'
   devise_for :users
   root to: 'resorts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
