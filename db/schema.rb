0864c1d5bbe857b303e065136bd97276644aec7c
Neringa Daniulaityte <neringa.daniulaityte@gmail.com>
2018-08-02 12:22:52 +0800

    preparing for merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_08_01_102607) do
-
+ActiveRecord::Schema.define(version: 2018_08_01_084152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +62,6 @@ ActiveRecord::Schema.define(version: 2018_08_01_102607) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "website"
     t.index ["user_id"], name: "index_resorts_on_user_id"
   end
 
