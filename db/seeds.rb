c8bc11fd67d11e2ebdeca1f190f9caefad480e91
Leanne Egan <lmegan92@gmail.com>
2018-08-03 14:49:20 +0800

    edited seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,5 +27,5 @@ resort2.features << Feature.all.sample
 resort3.features << Feature.all.sample
 resort4.features << Feature.all.sample
 
-booking1 = Booking.create(start_date: "July", duration: 6, total_price: 120, user: user1, resort: resort1)
+booking1 = Booking.create(start_date: "July", duration: 6, price: 120, user: user1, resort: resort1)
 
