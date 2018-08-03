0da8bb119ac4a8cdd0c1d8f6c32172fec05cbfa1
Leanne Egan <lmegan92@gmail.com>
2018-08-03 16:24:16 +0800

    updating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Booking.destroy_all
 Feature.destroy_all
 Resort.destroy_all
 User.destroy_all
 
 
-SLEEP_TIME = 3
+SLEEP_TIME = 10
 
 user1 = User.create!(email:'email@email.com', password:'password', is_owner: "False")
 
