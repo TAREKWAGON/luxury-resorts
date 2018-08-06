ba70aa3925eaa45ad29fe8e46557856a0de2a11a
tanguy Colou <tanguy.colou@skema.edu>
2018-08-06 17:58:21 +0800

    fav is done

--- app/controllers/favorite_resorts_controller.rb
+++ app/controllers/favorite_resorts_controller.rb
@@ -2,19 +2,28 @@ class FavoriteResortsController < ApplicationController
   before_action :set_resort
   skip_after_action :verify_authorized
 
+
   def create
     Favorite.create(favorited: @resort, user: current_user)
-    redirect_to @resort
+    redirect_back(fallback_location: "")
+    # if current_page?(resorts_path)
+    #   redirect_to resorts_path
+    # else
+    #   redirect_to resorts_path(@resort)
+    # end
   end
 
   def destroy
     Favorite.where(favorited_id: @resort.id, user_id: current_user.id).first.destroy
-    redirect_to @resort
+    redirect_back(fallback_location: "")
+    # if current_user
+    #   redirect_to resorts_path
+    # end
   end
 
-  private
+    private
 
-  def set_resort
-    @resort = Resort.find(params[:resort_id])
-  end
+    def set_resort
+      @resort = Resort.find(params[:resort_id])
+    end
 end
