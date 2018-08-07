b0a242c5ee220e6940aa57e60017288b180fdd65
Leanne Egan <lmegan92@gmail.com>
2018-08-07 15:18:30 +0800

    change fav res controller

--- app/controllers/favorite_resorts_controller.rb
+++ app/controllers/favorite_resorts_controller.rb
@@ -9,8 +9,11 @@ class FavoriteResortsController < ApplicationController
 
 
   def create
-    Favorite.create(favorited_id: @resort.id, user: current_user)
-    redirect_back(fallback_location: "")
+    if Favorite.create(favorited_id: @resort.id, user: current_user)
+      redirect_to @resort
+    else
+      redirect_to resorts_path
+    end
     # if current_page?(resorts_path)
     #   redirect_to resorts_path
     # else
@@ -20,7 +23,7 @@ class FavoriteResortsController < ApplicationController
 
   def destroy
     Favorite.where(favorited_id: @resort.id, user_id: current_user.id).first.destroy
-    redirect_back(fallback_location: "")
+    redirect_to @resort
     # if current_user
     #   redirect_to resorts_path
     # end
